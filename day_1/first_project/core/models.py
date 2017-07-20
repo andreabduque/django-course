6a564f7feb9d962ed0936bc1940ffb23c4666d61
Andrea Brandao Duque <abd@ug4c22.windows.cin.ufpe.br>
2017-07-20 16:12:24 -0300

    Day 3

--- day_1/first_project/core/models.py
+++ day_1/first_project/core/models.py
@@ -10,4 +10,29 @@ class Curso(models.Model):
 		verbose_name_plural = "Cursos"
 
 	def __str__(self):
+		return '{0} - {1}'.format(self.nome, self.codigo)
+
+class Aluno(models.Model):
+	nome = models.CharField(max_length=40)
+	email = models.EmailField(max_length=40)
+	cpf = models.CharField(max_length=14)
+	curso = models.ForeignKey('Curso')
+	rank = models.DecimalField(max_digits=3, decimal_places=2)
+	data_nascimento = models.DateField()
+
+	def __str__(self):
 		return '{0}'.format(self.nome)
+
+class Disciplina(models.Model):
+	nome = models.CharField(max_length=100)
+	codigo = models.CharField(max_length=5)
+	curso = models.ForeignKey('Curso')
+	data_de_insercao = models.DateField()
+
+	tipo_eletiva = models.CharField(
+        max_length=15,
+        choices= (('perfil', 'Perfil'), ('livre', 'Livre'), ('obrigatoria', 'Obrigatoria')),
+        default='obrigatoria'
+    )
+
+   
